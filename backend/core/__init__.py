c4f6c13114c70600dbb23944f054f77c5b41d038
akbyhakan <akby.hakan@gmail.com>
2025-12-16 04:00:48 +0300

    Create backend/core/__init__.py

---
+++ backend/core/__init__.py
@@ -0,0 +1,12 @@
+from .browser import browser_manager, BrowserManager
+from .captcha_solver import CaptchaSolver
+from .proxy_manager import ProxyManager
+from .session_manager import SessionManager
+
+__all__ = [
+    "browser_manager",
+    "BrowserManager", 
+    "CaptchaSolver",
+    "ProxyManager",
+    "SessionManager"
+]
